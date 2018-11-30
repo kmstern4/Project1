dc98969cefd6307c8468696c908eacf035a23809
Jeremy Parra <jeremydeanparra@gmail.com>
2018-11-30 13:49:21 -0700

    added functionality to js page

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -3,6 +3,7 @@ var geoLocation;
 var cinemaLocation;
 var lat;
 var long;
+var showTimes = [];
 
 //on click function for submit button
 $("#submit").on("click", function () {
@@ -11,37 +12,46 @@ $("#submit").on("click", function () {
     var location = $("#input-location").val().trim();
     console.log(date);
 
-    //Geocode ajax to convert address to lat and long
+    //Geocode ajax to convert address to lat and long, not currently being used
     $.ajax({
         url: "https://maps.googleapis.com/maps/api/geocode/json?address=" + location + "&key=AIzaSyC7cYVBuDvtxBDLzn62sHS0VlrDUf-WJMU",
         method: "GET"
     }).then(function (response) {
         console.log(response.results[0].geometry.location);
 
+        //variables below originally used but no longer serve function. delete if we dont find use
         lat = response.results[0].geometry.location.lat;
         long = response.results[0].geometry.location.lng;
         geoLocation = lat + ";" + long;
     })
 
-    //This is the ajax call on gracenote API using the lat and long variables
+    //This is the ajax call on gracenote API using the location variables
     $.ajax({
         url: "https://data.tmsapi.com/v1.1/movies/showings?startDate=" + date + "&zip=" + location + "&api_key=ebxmggvfebvqkmhczkwvzxk4",
         method: "GET"
     }).then(function (response) {
         console.log(response);
 
-        var movieArray = response.films;
+        //prints 10 buttons of movie selections
+        var movieArray = response;
             for (var i = 0; i < 10; i++) {
                 var filmName = movieArray[i].title;
                 var theater = movieArray[i].showtimes[0].theatre.name;
-                var newButton=$("<button>").text(filmName).attr("id", theater).addClass("film-button");
-                $("#submit").append(newButton);
-
+                var showtime = movieArray[i].showtimes[0].dateTime;
+                showTimes.push(showtime);
+                var newButton=$("<button>").text(filmName).attr("id", theater).attr("data-date", showtime).addClass("film-button");
+                $("#test-div").append(newButton);
             }
     });
-
-
 });
+//testing: this is to append the 2nd stage where movie buttons are replaced by selected movies, associated theater and show times, still needs work.
+$(document).on("click", ".film-button", function () {
+        var theater = $(this).attr("id");
+        var time = $(this).attr("data-date");
+        $("test-div").empty();
+        console.log(showTimes);
+});
+
 
 
 
