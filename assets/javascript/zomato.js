10031a5b580e22ce123c73b8d1b2c3645bdf8b50
dewyoyo <dewyoyo@hotmail.com>
2018-11-30 18:37:52 -0700

    copy app.js into chris_app.js and change .text(showTimes[0].substr(11, 6))

--- assets/javascript/zomato.js
+++ assets/javascript/zomato.js
@@ -11,20 +11,6 @@ $("#submit").on("click", function () {
     var location = $("#input-location").val().trim();
     console.log(location);
 
-    // var url = "https://cors-anywhere.herokuapp.com/https://api-gate.movieglu.com/filmsNowShowing/?n=10",
-    // url: url + "/geocode",
-    // headers: zheader,
-    // data: {
-    //     lat: coords.latitude,
-    //     lon: coords.longitude
-    // },
-    // zheader = {
-    //     Accept: "text/plain; charset=utf-8",
-    //     "Content-Type": "text/plain; charset=utf-8",
-    //     "X-Zomato-API-Key": "10bbf65b13ae378a2323cf3b8c13c49f"
-    // };
-
-
     var queryURL = "https://developers.zomato.com/api/v2.1/geocode?lat=40.742051&lon=-74.004821" ;
 
     var settings = {
